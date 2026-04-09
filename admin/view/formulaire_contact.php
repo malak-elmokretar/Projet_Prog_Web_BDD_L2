0057100fd5f865e10894f687098266d75ae896b9
malak-elmokretar <169133290+malak-elmokretar@users.noreply.github.com>
2026-04-09 15:37:27 +0200

    added email

--- admin/view/formulaire_contact.php
+++ admin/view/formulaire_contact.php
@@ -4,6 +4,16 @@
 			<label for="name" class="form-label">Nom</label>	
 			<input type="text" class="form-control" id="name" name="name" required>	
 		</div>
+
+		<div class="mb-3">
+			<label for="prenom" class="form-label">Prénom</label>	
+			<input type="text" class="form-control" id="prenom" name="prenom" required>	
+		</div>
+
+		<div class="mb-3">
+			<label for="objet" class="form-label">Objet</label>	
+			<input type="text" class="form-control" id="objet" name="objet" required>	
+		</div>
 	
 		<div class="mb-3">
 			<label for="mail" class="form-label">Adresse Mail</label>	
@@ -12,8 +22,7 @@
 	
 		<div class="form-floating mb-3">
 			<label for="message" class="form-label">Message :</label>
-			<textarea class="form-control long_texte" id="message" required></textarea>
-			<label for="floatingTextarea2">Message</label>
+			<textarea class="form-control long_texte" id="message" name="message" required></textarea>
 		</div>
 	
 		<div class="d-grid">
