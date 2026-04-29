a56f9a60b4803c55c2a4ac4269f2612a92dab2cc
thais-es <estebanthais8@gmail.com>
2026-04-29 18:53:24 +0200

    profil ok + deconexion

--- view/info_user.php
+++ view/info_user.php
@@ -40,4 +40,8 @@
     <button type="submit" class="btn btn-danger mt-3">Supprimer mon compte</button>
   </form>
 
+  <form action="<?php echo $racine_path; ?>control/deconnexion.php" method="POST">
+    <button type="submit" class="btn btn-danger">Se déconnecter</button>
+  </form>
+  
 </div>
